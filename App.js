867697bf6bd0b9bd97390fe3c82b31699ab8bda7
camilo17 <cami172013@gmail.com>
2018-06-10 19:32:01 -0500

    Removed the outer container over our app

--- App.js
+++ App.js
@@ -5,18 +5,10 @@ import InstaClone from './src/InstaClone';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+     
         <InstaClone/>
-      </View>
+     
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
