0897716d52107db613c4642f15a42b6dc0f0ef2f
camilo17 <cami172013@gmail.com>
2018-06-11 17:28:35 -0500

    created a bottomTabNavigator and used it in our main stack

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native'
-import {MainFeed, Login} from './components/screens'
-import {createStackNavigator, TabNavigator} from 'react-navigation';
+import {MainFeed, Login, Camera, Profile} from './components/screens'
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 
+
+const Tabs = createBottomTabNavigator({
+  feed: MainFeed,
+  camera: Camera,
+  profile: Profile
+})
 
 const MainStack = createStackNavigator({
   login: {
     screen: Login
   },
   main: {
-    screen: MainFeed
+    screen: Tabs
   }
 }, {
-  initialRouteName: 'main'
+  initialRouteName: 'login'
 })
 export default class InstaClone extends Component {
     
