a90f59ab978191c5944d00e49bb7b788ef9de520
camilo17 <cami172013@gmail.com>
2018-06-11 17:23:23 -0500

    Exported our dummy Camera and profile components

--- src/components/screens/index.js
+++ src/components/screens/index.js
@@ -1,7 +1,11 @@
 import MainFeed from './MainFeed';
 import Login from './Login';
+import Camera from './Camera';
+import Profile from './Profile';
 
 export {
     MainFeed,
-    Login
+    Login,
+    Camera,
+    Profile
 }
