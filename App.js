e03c6aec35521dacaf33046d5428cd4f5c661739
camilo17 <cami172013@gmail.com>
2018-06-10 19:05:27 -0500

    Use the InstaClone component for our app

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import InstaClone from './src/InstaClone';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <InstaClone/>
       </View>
     );
   }
