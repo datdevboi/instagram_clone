79ec0acd9727fc0f6cc0f54a356a2f9ee2f96bcc
camilo17 <cami172013@gmail.com>
2018-06-11 17:28:52 -0500

    Fixed an issue with our top nav

--- src/components/screens/MainFeed.js
+++ src/components/screens/MainFeed.js
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     tempNav: {
         width: 100 + "%",
         height: 56,
-        marginTop: 20,
+       
         backgroundColor: "rgb(250,250,250)",
         borderBottomColor: "rgb(233,233,233)",
         borderBottomWidth: StyleSheet.hairlineWidth,
