5f968444f0e7cd7f7a70c4c91b93f2cc79841026
camilo17 <cami172013@gmail.com>
2018-06-11 11:56:03 -0500

    got some images and exported them them in a config file

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+const icons =  {
+    images: {
+        heart: require('../../assets/heart-outline-50.png'),
+        message: require("../../assets/speech-bubble-50.png"),
+        arrow: require("../../assets/forward-arrow-50.png"),
+        menu: require("../../assets/menu-vertical-48.png")
+    }
+}
+
+
+
+export default icons;
