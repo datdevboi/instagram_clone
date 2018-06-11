423fe5020125956062890e0c7a98a1ca3be27f63
camilo17 <cami172013@gmail.com>
2018-06-11 17:23:04 -0500

    Made basic component

---
+++ src/components/screens/Camera.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {  View, Text, } from 'react-native';
+
+export default class Camera extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Camera </Text>
+      </View>
+    );
+  }
+}
