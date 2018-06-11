7f702f8c1e3013d81aacaec7d359ed29a99af6f9
camilo17 <cami172013@gmail.com>
2018-06-11 17:10:57 -0500

    Created a MainStack component using react navigator

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, Image, Dimensions, TouchableOpacity } from 'react-native'
-import {MainFeed} from './components/screens'
+import {MainFeed, Login} from './components/screens'
+import {createStackNavigator, TabNavigator} from 'react-navigation';
+
+
+const MainStack = createStackNavigator({
+  login: {
+    screen: Login
+  },
+  main: {
+    screen: MainFeed
+  }
+}, {
+  initialRouteName: 'main'
+})
 export default class InstaClone extends Component {
     
 
@@ -9,7 +22,7 @@ export default class InstaClone extends Component {
    
     return (
       
-        <MainFeed/>
+        <MainStack/>
       
     )
   }
