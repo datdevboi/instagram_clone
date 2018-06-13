8e7029543871a989817d177f1e06e12b03ac4e96
camilo17 <cami172013@gmail.com>
2018-06-12 19:39:36 -0500

    Added more functionality to our Register component

--- src/components/screens/Register.js
+++ src/components/screens/Register.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, TextInput, Button } from 'react-native'
+
 
 export default class Register extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
 
-  login = () => {
-    this.props.navigation.navigate("main")
+  register = () => {
+    // this.props.navigation.navigate("main")
   }  
+
+  updateText = (fieldName, newText) => {
+    this.setState({
+      [fieldName]: newText
+    })
+  }
   render() {
     return (
-      <View style={styles.loginContainer}>
-      <TouchableOpacity 
-        onPress={this.login}
-        >
-         <Text> Register </Text>
-      </TouchableOpacity>
+      <View style={styles.registerContainer}>
+
+        <Text>Register Page</Text>  
+        
+        <TextInput 
+         autoCorrect={false}
+          onChangeText={ (text) => {
+            this.updateText("email", text)
+          }}style={styles.input} 
+          placeholder="email" 
+          value={this.state.email}
+        />
+        <TextInput 
+          autoCorrect={false}
+          onChangeText={ (text) => {
+            this.updateText("password", text)
+          }}
+          style={styles.input} secureTextEntry placeholder="password" value={this.state.password}/>
+        <Button onPress={this.register} title="register"/>
+        
+
+      
+      
       </View>
     )
   }
@@ -22,11 +50,26 @@ export default class Register extends Component {
 
 
 const styles = StyleSheet.create({
-    loginContainer: {
+    registerContainer: {
         height: 100 + '%',
         width: 100 + '%',
         flex: 1,
         justifyContent: "center",
         alignItems: 'center',
+        backgroundColor: "rgb(252,61,57)",
+
+    },
+    input: {
+     
+      height: 50,
+      width: 100 + "%",
+      backgroundColor: "white",
+      marginBottom: 10,
+      
+    },
+    form: {
+      flexDirection: 'column',
+      justifyContent: "center",
+      alignItems: 'flex-start',
     }
 })
