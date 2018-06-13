b232fab6537df1194bc6a15140014f8e81f3f71c
camilo17 <cami172013@gmail.com>
2018-06-13 18:27:30 -0500

    Reused most of the code from register to let user login and made a button to redirect users to the Register page

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,19 +1,76 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, TextInput, Button } from 'react-native'
+import config from '../../config';
 
 export default class Login extends Component {
 
+  state = {
+    email: '',
+    password: ''
+  }
+
   login = () => {
-    this.props.navigation.navigate("signup")
+    
+    fetch(config.baseUrl + '/api/login', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    email: this.state.email,
+    password: this.state.password,
+  }),
+})
+  .then(data => data.json())
+  .then(jsondata => {
+    if(jsondata.confirmation === "success") {
+      this.props.navigation.navigate("main")
+    } else {
+      throw new Error({message: "Sorry something went wrong with register"})
+    }
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
   }  
+
+  updateText = (fieldName, newText) => {
+    this.setState({
+      [fieldName]: newText
+    })
+  }
+
+  toRegister = () => {
+    this.props.navigation.navigate("register");
+  }
   render() {
     return (
-      <View style={styles.loginContainer}>
-      <TouchableOpacity 
-        onPress={this.login}
-        >
-         <Text> Login Page </Text>
-      </TouchableOpacity>
+      <View style={styles.registerContainer}>
+
+        <Text>Login Page</Text>  
+        
+        <TextInput 
+         autoCorrect={false}
+          onChangeText={ (text) => {
+            this.updateText("email", text)
+          }}style={styles.input} 
+          placeholder="email" 
+          value={this.state.email}
+        />
+        <TextInput 
+          autoCorrect={false}
+          onChangeText={ (text) => {
+            this.updateText("password", text)
+          }}
+          style={styles.input} secureTextEntry placeholder="password" value={this.state.password}/>
+        <Button onPress={this.login} title="login"/>
+
+        <Button title="Register" onPress={this.toRegister}/>
+        
+
+      
+      
       </View>
     )
   }
@@ -22,11 +79,26 @@ export default class Login extends Component {
 
 
 const styles = StyleSheet.create({
-    loginContainer: {
+    registerContainer: {
         height: 100 + '%',
         width: 100 + '%',
         flex: 1,
         justifyContent: "center",
         alignItems: 'center',
+        backgroundColor: "rgb(252,61,57)",
+
+    },
+    input: {
+     
+      height: 50,
+      width: 100 + "%",
+      backgroundColor: "white",
+      marginBottom: 10,
+      
+    },
+    form: {
+      flexDirection: 'column',
+      justifyContent: "center",
+      alignItems: 'flex-start',
     }
 })
