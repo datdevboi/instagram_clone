1620c06b83ca47e7900da55fcc4f088af831e026
camilo17 <cami172013@gmail.com>
2018-06-21 13:13:32 -0500

    made some fixes

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,15 @@
-const config =  {
-    images: {
-        heartIcon: require('../../assets/heart-outline-50.png'),
-        messageIcon: require("../../assets/speech-bubble-50.png"),
-        arrowIcon: require("../../assets/forward-arrow-50.png"),
-        menuIcon: require("../../assets/menu-vertical-48.png")
-    },
-    styleConstants: {
-        rowHeight: 50
-    },
-    baseUrl: 'http://instagram_clone-h9somf.turbo360-vertex.com/'
-}
-
-
+const config = {
+  images: {
+    heartIcon: require('../../assets/heart-outline-50.png'),
+    messageIcon: require('../../assets/speech-bubble-50.png'),
+    arrowIcon: require('../../assets/forward-arrow-50.png'),
+    menuIcon: require('../../assets/menu-vertical-48.png'),
+  },
+  styleConstants: {
+    rowHeight: 50,
+  },
+  baseUrl: 'http://instagram_clone-h9somf.turbo360-vertex.com/',
+  devUrl: 'http://localhost:3000/api',
+};
 
 export default config;
