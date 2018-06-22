7149200e89408235b6fe6a0d79c53231b84b48ae
camilo17 <cami172013@gmail.com>
2018-06-22 08:10:44 -0500

    Get users pictures and renderer them

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
+import config from '../../config';
 import withContext from '../../withContext';
 
 class P extends Component {
+  state = {
+    profilePics: [],
+  };
+
+  static oneThirdWdith = Math.floor(Dimensions.get('window').width / 3);
+  componentDidMount() {
+    fetch(config.baseUrl + `/api/photo?user=${this.props.context.userId}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          profilePics: data.data,
+        })
+      )
+      .catch(err => alert(err));
+  }
   render() {
     return (
-      <View>
-        <Text> Profile </Text>
+      <View style={styles.container}>
+        {this.state.profilePics.map((pic, i) => {
+          return (
+            <Image
+              style={styles.profilePic}
+              source={{ uri: `${pic.url}=s${P.oneThirdWdith}-c` }}
+              key={i}
+            />
+          );
+        })}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100 + '%',
+    flexDirection: 'row',
+  },
+  profilePic: {
+    height: P.oneThirdWdith,
+    width: P.oneThirdWdith,
+  },
+});
 
 const Profile = withContext(P);
 
