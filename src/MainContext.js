cee920e67448621b5d5d599c879794ffbd69bff8
camilo17 <cami172013@gmail.com>
2018-06-22 09:07:32 -0500

    Added a few more properties and methods to our MainContext

--- src/MainContext.js
+++ src/MainContext.js
@@ -11,6 +11,8 @@ export class MainContext extends Component {
 
     this.state = {
       userId: '',
+      profilePics: [],
+      fetchedPics: false,
     };
   }
 
@@ -19,12 +21,37 @@ export class MainContext extends Component {
       userId: newId,
     });
   };
+
+  updatePhotos = photos => {
+    this.setState({
+      profilePics: photos,
+      fetchedPics: true,
+    });
+  };
+
+  newPhoto = photo => {
+    if (this.state.fetchedPics === true) {
+      if (this.state.profilePics.length === 0) {
+        this.setState({
+          profilePics: [photo],
+        });
+      } else {
+        this.setState({
+          profilePics: [...this.state.profilePics, photo],
+        });
+      }
+    }
+  };
+
   render() {
     return (
       <Provider
         value={{
           userId: this.state.userId,
+          profilePics: this.state.profilePics,
           changeUserId: this.changeUserId,
+          updatePhotos: this.updatePhotos,
+          newPhoto: this.newPhoto,
         }}>
         {this.props.children}
       </Provider>
